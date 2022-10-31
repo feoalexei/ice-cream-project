64d34bb3c849c7d864d4cf5a73f5118bfcbf1af6
DanilVelichko <velichko.danil@gmail.com>
2022-10-31 18:28:16 +0200

    all product done

--- src/js/products-modal.js
+++ src/js/products-modal.js
@@ -2,13 +2,15 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector("[data-products-modal-open]"),
-    // openModalBtnMob: document.querySelector("[data-products-modal-open-mob]"),
+    openModalBtn2: document.querySelector("[data-products-modal-open2]"),
+    openModalBtn3: document.querySelector("[data-products-modal-open3]"),
     closeModalBtn: document.querySelector("[data-products-modal-close]"),
     modal: document.querySelector("[data-products-modal]"),
   };
 
   refs.openModalBtn.addEventListener('click', remove);
-  // refs.openModalBtnMob.addEventListener('click', remove);
+  refs.openModalBtn2.addEventListener('click', remove);
+  refs.openModalBtn3.addEventListener('click', remove);
   refs.closeModalBtn.addEventListener("click", add);
 
   function remove() {
